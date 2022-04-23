e1cfb2d0abf40287edb377b792c4ce206b68b45c
Batey Josue <josuebatey19@gmail.com>
2022-04-23 19:21:55 +0200

    adding script file with initial code of active button on click function

---
+++ apps.js
@@ -0,0 +1,17 @@
+
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+pageTransition = () =>{
+    for(let i = 0; i < sectBtn.length; i++)
+    {
+        sectBtn[i].addEventListener('click',function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+}
+pageTransition();
